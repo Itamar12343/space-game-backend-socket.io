41c0f8e42647c2d186f246ebff7151d2eb0276dd
Itamar1234 <Itamardarihess@gmail.com>
2023-02-09 15:51:51 +0200

    my first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const cors = require("cors");
+const port = process.env.Port || 3001;
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+io.on("connection", socket => {
+
+    socket.on("stream", data => {
+        socket.broadcast.emit("stream", data);
+    });
+
+
+});
+
+server.listen(port, () => {
+    //console.log("success");
+});
